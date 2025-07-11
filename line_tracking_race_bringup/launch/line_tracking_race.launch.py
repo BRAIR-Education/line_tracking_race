4b344f0e9cd96e27f01f2e32faca957214fc9f3c
Ugo Albanese <u.albanese@santannapisa.it>
2025-07-11 19:08:37 +0200

    Add ros2_control support via 'use_ros2_control' launch argument. It provides an alternative Differential drive control to the Gazebo plugin (i.e. DiffDrive)

--- line_tracking_race_bringup/launch/line_tracking_race.launch.py
+++ line_tracking_race_bringup/launch/line_tracking_race.launch.py
@@ -4,33 +4,109 @@ from launch import LaunchDescription
 from launch.actions import (
     DeclareLaunchArgument,
     IncludeLaunchDescription,
+    OpaqueFunction,
+    RegisterEventHandler
 )
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition 
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution, 
+    Command
+)
 
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node
+
+
+pkg_project_bringup = get_package_share_directory("line_tracking_race_bringup")
+pkg_project_gazebo = get_package_share_directory("line_tracking_race_gazebo")
+pkg_project_description = get_package_share_directory("line_tracking_race_description")
+    
+pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+def generate_ros_gz_bridge_description(context, *args, **kwargs):
+
+    # Get launch configuration values
+    use_ros2_control = LaunchConfiguration('use_ros2_control').perform(context)  # actual value
+
+    ros_gz_bridge_conf_file = f"ros_gz_bridge_{"ros2" if use_ros2_control.lower() == "true" else "gazebo"}_control.yaml"
+    
+    gz_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': PathJoinSubstitution([pkg_project_bringup, "config", ros_gz_bridge_conf_file]),
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        }],
+        output='screen'
+    )
+    
+    return [gz_bridge]
+
+def generate_ros2_control_description(context, *args, **kwargs):
+
+    use_ros2_control = LaunchConfiguration('use_ros2_control').perform(context) # actual value
+    car_ros2_control_file = LaunchConfiguration("car_ros2_control_file")
+
+    ld = LaunchDescription()
+
+    if use_ros2_control.lower() == "true":
+        joint_state_broadcaster_spawner = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['joint_state_broadcaster'],
+        )
+        ld.add_action(joint_state_broadcaster_spawner)
+        
+        car_controllers_conf_file = PathJoinSubstitution([pkg_project_bringup,
+                                                          "config",
+                                                          car_ros2_control_file])
+        
+        diff_drive_base_controller_spawner = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=[
+                'diff_drive_base_controller',
+                '--param-file',
+                car_controllers_conf_file,
+                '--controller-ros-args',
+                '-r /diff_drive_base_controller/cmd_vel:=/cmd_vel',
+                '--controller-ros-args',
+                '-r /diff_drive_base_controller/odom:=/odom',
+                ],
+        )
+        ld.add_action(RegisterEventHandler(OnProcessExit(target_action=joint_state_broadcaster_spawner,
+                                                        on_exit=[diff_drive_base_controller_spawner])))
+
+    return [ld]
+    
 def generate_launch_description():
 
-    # Setup project paths
-    pkg_project_bringup = get_package_share_directory("line_tracking_race_bringup")
-    pkg_project_gazebo = get_package_share_directory("line_tracking_race_gazebo")
-    pkg_project_description = get_package_share_directory("line_tracking_race_description")
-    
-    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
-
     # Declare launch arguments
-
-    world_file = DeclareLaunchArgument(
+    world_file_arg = DeclareLaunchArgument(
         'world_file',
         default_value='race_track.world',
         description='The world file'
     )
 
-    car_file = DeclareLaunchArgument(
+    car_file_arg = DeclareLaunchArgument(
         'car_file',
         default_value='car.urdf.xacro',
         description='The car model file (xacro/urdf/sdf)'
+    )
+
+    car_ros2_control_file_arg = DeclareLaunchArgument(
+        'car_ros2_control_file',
+        default_value='car_control_diff_drive.yaml',
+        description='The car ros2_control configuration yaml file (yaml)'
+    )
+
+    use_ros2_control_arg = DeclareLaunchArgument(
+        'use_ros2_control',
+        default_value='false',
+        description='Use ros2_control instead of gazebo control plugin'
     )
 
     x_pos_arg = DeclareLaunchArgument(
@@ -46,9 +122,8 @@ def generate_launch_description():
         "yaw", default_value="3.142", description="Yaw orientation to spawn the robot"
     )
 
-    # rviz_arg = DeclareLaunchArgument(
-    #     'rviz', default_value='true', description='Open RViz.'
-    # )
+    rviz_arg = DeclareLaunchArgument(
+        'rviz', default_value='true', description='Open RViz.')
     
     # --------- Launch Gazebo server and ros_gz_bridge with ros_gz_sim.launch ---------
     # https://github.com/gazebosim/ros_gz/blob/ros2/ros_gz_sim/launch/ros_gz_sim.launch.py
@@ -82,46 +157,38 @@ def generate_launch_description():
     # )
 
     # launch gazebo
+    world_file = PathJoinSubstitution([pkg_project_gazebo, 'worlds', LaunchConfiguration("world_file")])
+
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
         ),
         launch_arguments={
-            'gz_args': [PathJoinSubstitution([pkg_project_gazebo, 'worlds', LaunchConfiguration("world_file")]), " -v"],
+            # -v -> verbose
+            # -r -> start simulation (required by ros2_control) 
+            'gz_args': [world_file, " -r -v 1"], 
             'on_exit_shutdown': 'True'
         }.items(),
     )
+
     # Launch bridge separately, avoiding ros_gz_bridge.launch,
     # using the executable ros_gz_bridge/parameter_bridge
-    gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': PathJoinSubstitution([pkg_project_bringup, "config", "ros_gz_bridge.yaml"]),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-        }],
-        output='screen'
-    )
+    # Its configuration depends on the the type of control (gazebo, ros2_control) 
+    gz_bridge = OpaqueFunction(function=generate_ros_gz_bridge_description)
 
     # --------- Load car xacro/urdf ---------
-    # Start a robot_state_publisher node publishing a "robot_description" topic
-    models_path = PathJoinSubstitution([pkg_project_description, "models"])
+    urdf_path = PathJoinSubstitution([PathJoinSubstitution([pkg_project_description, "models"]),
+                                     "urdf",
+                                     LaunchConfiguration("car_file")])
 
-    car_urdf = IncludeLaunchDescription(
-        PathJoinSubstitution(
-            [
-                get_package_share_directory("urdf_launch"),
-                "launch",
-                "description.launch.py",
-            ]
-        ),
-        launch_arguments={
-            "urdf_package": "line_tracking_race_description",
-            "urdf_package_path": PathJoinSubstitution(
-                [models_path, "urdf", LaunchConfiguration("car_file")]
-            ),
-        }.items(),
-    )
+    robot_description_content = Command(['xacro ', urdf_path,
+                                         ' use_ros2_control:=', LaunchConfiguration("use_ros2_control")])
+
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[{
+                                          'robot_description': ParameterValue(robot_description_content, value_type=str),
+                                      }])
 
     # --------- Spawn Robot node ---------
     spawn_robot_node = Node(
@@ -138,29 +205,37 @@ def generate_launch_description():
         output="screen",
     )
 
+    # spawn ros2_control-related nodes AFTER spawn_robot_node
+    ros2_control_description = OpaqueFunction(function=generate_ros2_control_description)
+    ros2_control = RegisterEventHandler(OnProcessExit(target_action=spawn_robot_node,
+                                                      on_exit=[ros2_control_description]))
+
     # Visualize in RViz
     rviz = Node(
        package='rviz2',
        executable='rviz2',
        arguments=['-d', PathJoinSubstitution([pkg_project_bringup, "config", 'line_tracking_race.rviz'])],
-       condition=IfCondition(LaunchConfiguration('rviz_arg'))
+       condition=IfCondition(LaunchConfiguration('rviz'))
     )
 
     return LaunchDescription(
         [
-            world_file,
-            car_file,
+            world_file_arg,
+            car_file_arg,
             x_pos_arg,
             y_pos_arg,
             z_pos_arg,
             yaw_arg,
+            use_ros2_control_arg,
+            car_ros2_control_file_arg,
             # rviz_arg,
             # gz_sim_ros_bridge,
             # gz_gui_cmd,
             gz_sim,
             gz_bridge,
-            car_urdf,
+            robot_state_publisher,
             spawn_robot_node,
+            ros2_control
             # rviz
         ]
     )
